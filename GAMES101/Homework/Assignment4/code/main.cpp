5d2743bc936d5c52b6c17c56b97d79b620ffa535
Odeinjul <odeinjul@gmail.com>
2022-08-10 16:43:02 +0800

    Finish GAMES101-pa4-antialiasing

--- GAMES101/Homework/Assignment4/code/main.cpp
+++ GAMES101/Homework/Assignment4/code/main.cpp
@@ -38,8 +38,8 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
     auto p_2 = control_points[2];
     auto p_3 = control_points[3];
 
-    auto p_return = std::pow(1 - t, 3) * p_0 + 3 * std::pow(1 - t, 2) * t * p_1 +
-            3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
+    auto p_return = pow(1 - t, 3) * p_0 + 3 * pow(1 - t, 2) * t * p_1 +
+            3 * pow(t, 2) * (1 - t) * p_2 + pow(t, 3) * p_3;
 
     return p_return;
 
@@ -49,10 +49,39 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
 {
     // Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's
     // recursive Bezier algorithm.
-    for(float t = 0; t <= 1; t +=0.001){
+    for(float t = 0; t <= 1; t +=0.001)
+    {
         auto point = recursive_bezier(control_points, t);
         window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
     }
+
+}
+
+void bezier_antialiasing(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
+{
+    // Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's
+    // recursive Bezier algorithm.
+    for(float t = 0; t <= 1; t +=0.001){
+        auto point = recursive_bezier(control_points, t);
+        int x_0 = int(point.x + 0.5), y_0 = int(point.y + 0.5);
+        std::vector<cv::Point2i> points = {
+                cv::Point2i(x_0, y_0), cv::Point2i(x_0 - 1, y_0),
+                cv::Point2i(x_0, y_0 - 1), cv::Point2i(x_0 - 1, y_0 - 1)
+        };
+        std::vector<float> distance;
+        float dis_sum = 0;
+        for(auto p : points) {
+            float x_temp = p.x + 0.5f, y_temp = p.y + 0.5f;
+            float dis_temp = sqrt(2) - sqrt(pow(x_temp - point.x, 2) + pow(y_temp - point.y, 2));
+            dis_sum += dis_temp;
+            distance.push_back(dis_temp);
+        }
+        for(int i = 0; i < 4; i ++) {
+            float k = distance[i] / dis_sum ;
+            window.at<cv::Vec3b>(points[i].y, points[i].x)[1] = std::min(255.f, 255.f * k + window.at<cv::Vec3b>(points[i].y, points[i].x)[1]);
+        }
+    }
+
 
 }
 
@@ -74,8 +103,8 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            bezier(control_points, window);
+            //naive_bezier(control_points, window);
+            bezier_antialiasing(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
